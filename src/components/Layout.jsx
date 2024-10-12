062d6132836dedb282096108e0fb7fca10803861
MartaPaszkowska <36098669+MartaPaszkowska@users.noreply.github.com>
2024-10-12 14:59:40 +0200

    1

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,11 +1,12 @@
-import { Suspense } from "react";
-import AppBar from "./AppBar";
+import { Suspense } from 'react';
+import { Outlet } from 'react-router-dom';
+import { AppBar } from './AppBar/AppBar';
 
-export default function Layout({ children }) {
-	return (
-		<div style={{ maxWidth: 960, margin: "0 auto", padding: "0 16px" }}>
-			<AppBar />
-			<Suspense fallback={null}>{children}</Suspense>
-		</div>
-	);
-}
+export const Layout = ({ children }) => {
+  return (
+    <div style={{ maxWidth: 960, margin: '0 auto', padding: '0 16px' }}>
+      <AppBar />
+      <Suspense fallback={null}>{children}</Suspense>
+    </div>
+  );
+};
