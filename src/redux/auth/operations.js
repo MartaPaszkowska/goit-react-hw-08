06dd41189071508e3acf7afe9902126fd2f1149a
MartaPaszkowska <36098669+MartaPaszkowska@users.noreply.github.com>
2024-10-13 19:05:12 +0200

    1

--- src/redux/auth/operations.js
+++ src/redux/auth/operations.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { createAsyncThunk } from "@reduxjs/toolkit";
 
-axios.defaults.baseURL = "https://task-manager-api.goit.global/";
+axios.defaults.baseURL = "https://connections-api.goit.global/docs/";
 
 // Utility to add JWT
 const setAuthHeader = (token) => {
